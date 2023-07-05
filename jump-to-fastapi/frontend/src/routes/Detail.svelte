ceb2cf2cd0975efab255c75a883a8e732bfca7be
binibin-k <binibinkk@gmail.com>
2023-07-06 06:29:42 +0900

    질문 상세조회 화면 및 API 구성

---
+++ jump-to-fastapi/frontend/src/routes/Detail.svelte
@@ -0,0 +1,20 @@
+<script>
+    import fastapi from "../lib/api"
+
+    export let params = {} // Detail 컴포넌트 호출 시 전달한 파라미터를 담을 변수
+    let question_id = params.question_id
+    let question = {} // 한 건의 질문에 대한 상세정보이므로 초기화 필요
+
+    function get_question() {
+        fastapi("get", "/api/question/detail/" + question_id, {}, (json) => {
+            question = json
+        })
+    }
+
+    get_question()
+</script>
+
+<h1>{question.subject}</h1>
+<div>
+    {question.content}
+</div>
