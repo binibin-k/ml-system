e11cc130bcc7d82a4c294fee94efa8c74f30453b
binibin-k <binibinkk@gmail.com>
2023-07-09 00:19:55 +0900

    부트스트랩 적용

--- jump-to-fastapi/frontend/src/components/Error.svelte
+++ jump-to-fastapi/frontend/src/components/Error.svelte
@@ -3,15 +3,17 @@
 </script>
 
 {#if typeof error.detail === 'string'} <!-- 일반 오류 -->
-    <ul>
-        <li>{error.detail}</li> <!-- 오류의 내용만 표시 -->
-    </ul>
+    <div class="alert alert-danger" role="alert">
+        <div>
+            {error.detail}
+        </div>
+    </div>
 {:else if typeof error.detail === 'object' && error.detail.length > 0} <!-- 필드 오류 -->
-    <ul>
-        {#each error.detail as err, i} <!-- 오류를 순회하면서 필드명과 필드 오류 출력 -->
-        <li>
+    <div class="alert alert-danger" role="alert">
+        {#each error.detail as err, i}
+        <div>
             <strong>{err.loc[1]}</strong> : {err.msg}
-        </li>
+        </div>
         {/each}
-    </ul>
+    </div>
 {/if}
--- jump-to-fastapi/frontend/src/main.js
+++ jump-to-fastapi/frontend/src/main.js
@@ -1,4 +1,9 @@
 import './app.css'
+
+// node_modules 디렉터리에 있는 파일은 상대경로 사용 가능
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 import App from './App.svelte'
 
 const app = new App({
